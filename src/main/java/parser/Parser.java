c7d4a01788229f7a9bd3fae8654e4ffa3100639f
illiasavenko7 <illiasavenko7@gmail.com>
2016-10-08 01:08:11 +0300

    test parsing

---
+++ src/main/java/parser/Parser.java
@@ -0,0 +1,52 @@
+package parser;
+
+import model.Attribute;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Parser implements ParserInterface {
+
+    private Attribute borderRadius;
+    private Attribute margin;
+
+    public Parser() {
+        borderRadius = new Attribute();
+        margin = new Attribute();
+        borderRadius.setName("border-radius");
+        borderRadius.setChromeSupport(true);
+        borderRadius.setIeSupport(false);
+        margin.setName("margin");
+        margin.setChromeSupport(true);
+        margin.setIeSupport(true);
+    }
+
+
+    public String parse(String input) {
+        Map<Attribute, Position> result = new HashMap<Attribute, Position>();
+
+        int firstIndex = input.indexOf(borderRadius.getName());
+        int lastIndex = firstIndex + borderRadius.getName().length();
+
+        result.put(borderRadius, new Position(firstIndex, lastIndex));
+        return result.toString();
+    }
+
+    private class Position {
+        private int start;
+        private int end;
+
+        Position(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public String toString() {
+            return "Position{" +
+                    "start=" + start +
+                    ", end=" + end +
+                    '}';
+        }
+    }
+}
