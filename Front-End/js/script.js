1c184aebb406022cc53952f46a2fd46a029d7d87
VadimDovgopol <electrovadim@gmail.com>
2016-10-13 21:13:52 +0300

    add validator: type file size file

---
+++ Front-End/js/script.js
@@ -0,0 +1,27 @@
+$(function () {
+
+    $('#file').bind('change', function(e) {
+        e.preventDefault();
+        var button = $('#button');
+        var size = this.files[0].size/1024/1024;
+        var maxSize = 1 ;
+        if(size > maxSize){
+            alert('error');
+            var input = $('#image-file');
+            input.replaceWith(input.val('').clone(true));
+        }
+
+        console.log('size', size);
+    });
+    $('INPUT[type="file"]').change(function () {
+        var ext = this.value.match(/\.(.+)$/)[1];
+        switch (ext) {
+            case 'css':
+                $('#Button').attr('disabled', false);
+                break;
+            default:
+                alert('This is not an allowed file type.');
+                this.value = '';
+        }
+    });
+});
