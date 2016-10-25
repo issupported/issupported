5ebae5ece524b921f03963418677d479c6742d1a
VadimDovgopol <electrovadim@gmail.com>
2016-10-26 00:21:35 +0300

    fix script

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -2,20 +2,20 @@ $(function () {
 
     $('#file').bind('change', function(e) {
         e.preventDefault();
+        const COEFFICIENT = 1024;
         var button = $('#button');
-        var size = this.files[0].size/1024/1024;//REVIEW: вот эти деления лучше вынести в константу и сделать коментарий к ней что это такое
-        var maxSize = 1 ;
-        if(size > maxSize){
-            alert('error'); //REVIEW:нормальное сообщение надо
-            var input = $('#image-file');
+        var sizeFile = this.files[0].size/COEFFICIENT/1024;
+        var maxSize = 1;
+        if(sizeFile > maxSize){
+            alert('to big file sizeFile');
+            var input = $('#file');
             input.replaceWith(input.val('').clone(true));
         }
-
-        console.log('size', size);
+        console.log('sizeFile', sizeFile);
     });
     $('INPUT[type="file"]').change(function () {
-        var ext = this.value.match(/\.(.+)$/)[1]; //REVIEW: название переменой не о чем не говорит
-        switch (ext) {
+        var fileType = this.value.match(/\.(.+)$/)[1];
+        switch (fileType) {
             case 'css':
                 $('#button').attr('disabled', false);
                 break;
@@ -24,5 +24,4 @@ $(function () {
                 this.value = '';
         }
     });
-    //REVIEW: баг с выводом ошибок, написал тебе в вк
 });
