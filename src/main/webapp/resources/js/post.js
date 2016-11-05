76de467bb19b73132dea295279f1199532546ac2
VadimDovgopol <electrovadim@gmail.com>
2016-11-05 17:41:03 +0200

    test ajax

---
+++ src/main/webapp/resources/js/post.js
@@ -0,0 +1,11 @@
+var myaap = angular.module('myapp', []);
+myaap.controller('mainCtrl', function ($http, $scope) {
+   http.post('/parse')
+       .success(function (result) {
+           console.log(result);
+       })
+        .error(function (result) {
+        console.log('error');
+
+    })
+});
