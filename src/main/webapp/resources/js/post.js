bed1dab6bef74305c3ca30b523b391942ae73082
VadimDovgopol <electrovadim@gmail.com>
2016-11-05 18:25:31 +0200

    test ajax

--- src/main/webapp/resources/js/post.js
+++ src/main/webapp/resources/js/post.js
@@ -2,7 +2,9 @@ function SendPost() {
 
     $.ajax({
         type:'post',
-        url:'/parse',
+        url:'http://192.168.1.60:8080/parse',
+        data: file,
+        dataType:'text',
         response:'json',
 
     success:function (data) {
