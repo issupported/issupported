90d0efc0696cb410f940c9fbbad8c66cbec925fb
illiasavenko <illiasavenko7@gmail.com>
2017-01-25 01:28:44 +0200

    db port change

--- src/main/java/com/issupported/model/BrowserSupported.java
+++ src/main/java/com/issupported/model/BrowserSupported.java
@@ -1,7 +1,12 @@
 package com.issupported.model;
 
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 @Entity
 @Table(name = "browser_to_attribute")
@@ -17,10 +22,16 @@ public class BrowserSupported implements Serializable {
     @ManyToOne
     @JoinColumn(name = "attribute_id", insertable = false, updatable = false)
     private Attribute attribute;
-
+    /*
     @Column(name = "supported")
     @Enumerated(EnumType.STRING)
-    private Supported supported;
+    private Supported supported;*/
+
+    @ElementCollection
+    @MapKeyColumn(name = "attribute_id")
+    @Column(name="supported") @Enumerated(EnumType.STRING)
+    //@CollectionTable(name="browser_to_attribute", joinColumns=@JoinColumn(name="browser_id"))
+    private Map<Attribute, Supported> attributeSupported = new HashMap<>();
 
     //TODO: store some statistics
 
@@ -28,60 +39,12 @@ public class BrowserSupported implements Serializable {
         return id;
     }
 
-    public Browser getBrowser() {
-        return browser;
-    }
-
-    public void setBrowser(Browser browser) {
-        this.browser = browser;
-    }
-
-    public Attribute getAttribute() {
-        return attribute;
-    }
-
-    public void setAttribute(Attribute attribute) {
-        this.attribute = attribute;
-    }
-
-    public Supported getSupported() {
-        return supported;
-    }
-
-    public void setSupported(Supported supported) {
-        this.supported = supported;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        BrowserSupported that = (BrowserSupported) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (browser != null ? !browser.equals(that.browser) : that.browser != null) return false;
-        if (attribute != null ? !attribute.equals(that.attribute) : that.attribute != null) return false;
-        return supported != null ? supported.equals(that.supported) : that.supported == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (browser != null ? browser.hashCode() : 0);
-        result = 31 * result + (attribute != null ? attribute.hashCode() : 0);
-        result = 31 * result + (supported != null ? supported.hashCode() : 0);
-        return result;
-    }
-
     @Override
     public String toString() {
         return "BrowserSupported{" +
                 "id=" + id +
                 ", browser=" + browser +
-                ", attribute=" + attribute +
-                ", supported='" + supported + '\'' +
+                ", attributeSupported=" + attributeSupported +
                 '}';
     }
 }
