9b963c59b155d31d356441b1d3c176bf4f40ac13
VadimDovgopol <electrovadim@gmail.com>
2017-01-27 18:07:26 +0200

    test new version of upload and send to server file

--- src/main/webapp/resources/js/myApp.js
+++ src/main/webapp/resources/js/myApp.js
@@ -19,8 +19,10 @@ myApp.directive('fileModel', ['$parse', function ($parse) {
 }]);
 
 
-
 myApp.controller('MainCtrl', ['$scope', 'fileUpload', function ($scope, fileUpload) {
+    $scope.showContent = function($fileContent){
+        $scope.content = $fileContent;
+    };
 
     $scope.setFile = function (element) {
 
@@ -67,6 +69,28 @@ myApp.controller('MainCtrl', ['$scope', 'fileUpload', function ($scope, fileUplo
     };
 
 }]);
+
+myApp.directive('onReadFile', function ($parse) {
+    return {
+        restrict: 'A',
+        scope: false,
+        link: function(scope, element, attrs) {
+            var fn = $parse(attrs.onReadFile);
+
+            element.on('change', function(onChangeEvent) {
+                var reader = new FileReader();
+
+                reader.onload = function(onLoadEvent) {
+                    scope.$apply(function() {
+                        fn(scope, {$fileContent:onLoadEvent.target.result});
+                    });
+                };
+
+                reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+            });
+        }
+    };
+});
 
 myApp.service('fileUpload', ['$http', function ($http) {
     this.uploadFileToUrl = function (file, uploadUrl) {
